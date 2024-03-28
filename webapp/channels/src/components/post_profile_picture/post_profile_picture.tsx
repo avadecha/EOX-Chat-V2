64794c0b94e86e9ae915504df6790a7bb6a88f94
Anuj Vadecha <avadecha@eoxvantage.com>
2024-03-28 15:02:40 -0400

    fix

--- webapp/channels/src/components/post_profile_picture/post_profile_picture.tsx
+++ webapp/channels/src/components/post_profile_picture/post_profile_picture.tsx
@@ -37,7 +37,7 @@ export default class PostProfilePicture extends React.PureComponent<Props> {
         if (user && user.id === post.user_id) {
             return Utils.imageURLForUser(user.id,user.username, user.is_bot);
         } else if (post.user_id) {
-            return Utils.imageURLForUser(user.id ,user.username, user.is_bot);
+            return Utils.imageURLForUser(post.user_id ,user?.username, user?.is_bot);
         }
 
         return '';
